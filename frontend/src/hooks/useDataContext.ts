c4a9df943d7e701148be6cfe62df4d98945fc518
7Sins <6631503046@lamduan.mfu.ac.th>
2025-12-21 21:34:00 +0700

    Add auto-refresh and fix session persistence

--- frontend/src/hooks/useDataContext.ts
+++ frontend/src/hooks/useDataContext.ts
@@ -6,11 +6,13 @@ import { MOCK_DATA } from '../constants';
 interface DataContextType {
   data: AppData;
   setData: React.Dispatch<React.SetStateAction<AppData>>;
+  refreshData: () => Promise<void>;
 }
 
 export const DataContext = React.createContext<DataContextType>({
   data: MOCK_DATA,
   setData: () => {},
+  refreshData: async () => {},
 });
 
 export const useDataContext = () => React.useContext(DataContext);
